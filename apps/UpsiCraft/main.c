99aef4d0dfc914aa849f34d55a7a4adf101581b0
Gauthier Cenes <cenes.gauthier@gmail.com>
2025-03-04 01:07:06 +0100

    feat(screen): optimize screen initialization and rendering logic with the background

--- apps/UpsiCraft/main.c
+++ apps/UpsiCraft/main.c
@@ -12,28 +12,24 @@ void extapp_main(void) {
     while (!extapp_isKeydown(5)) {
         if (extapp_isKeydown(3)) {
             cameraX++;
-            clearScreen();
             initScreen();
             mapToScreen();
             drawScreen();
         }
         if (extapp_isKeydown(0)) {
             cameraX--;
-            clearScreen();
             initScreen();
             mapToScreen();
             drawScreen();
         }
         if (extapp_isKeydown(2)) {
             cameraY++;
-            clearScreen();
             initScreen();
             mapToScreen();
             drawScreen();
         }
         if (extapp_isKeydown(1)) {
             cameraY--;
-            clearScreen();
             initScreen();
             mapToScreen();
             drawScreen();
--- apps/UpsiCraft/screen.c
+++ apps/UpsiCraft/screen.c
@@ -32,37 +32,23 @@ void drawSpriteCut(const uint8_t *texture, int16_t x, int16_t y, uint8_t cut) {
     }
     for (int j = 0; j < ISO_HEIGHT; j++) {
         for (int i = 0; i < ISO_WIDTH; i++) {
-            uint8_t colorIndex = texture[j * ISO_WIDTH + i];
-            uint8_t colorIndexCut = TEXTURE_BLOCKMAP[j * ISO_WIDTH + i];
 
+			uint8_t colorIndexCut = TEXTURE_BLOCKMAP[j * ISO_WIDTH + i];
+
+            if (texture == 0) {
+              	if (colorIndexCut == cut) {
+                    extapp_pushRectUniform(x + i, y + j, 1, 1, 0x555F);
+                  }
+                continue;
+            }
+
+            uint8_t colorIndex = texture[j * ISO_WIDTH + i];
             if (colorIndex != 255 && colorIndexCut == cut) {
                 uint16_t color = ((COLOR_PALETTE[colorIndex][0] >> 3) << 11) |
                                 ((COLOR_PALETTE[colorIndex][1] >> 2) << 5) |
                                 (COLOR_PALETTE[colorIndex][2] >> 3);
 
                 extapp_pushRectUniform(x + i, y + j, 1, 1, color);
-            }
-        }
-    }
-}
-
-void initScreen() {
-    for (uint8_t x = 0; x < 20; x++) {
-        for (uint8_t y = 0; y < 31; y++) {
-            SCREEN[x][y].block = 0;
-            SCREEN[x][y].face = 0;
-        }
-    }
-}
-
-void drawScreen() {
-    for (uint8_t x = 0; x < SCREEN_WIDTH; x++) {
-        for (uint8_t y = 0; y < SCREEN_HEIGHT; y++) {
-            uint8_t block = SCREEN[x][y].block;
-            uint8_t face = SCREEN[x][y].face;
-
-            if (block != 0) {
-                drawSpriteCut(TEXTURES[block - 1], x * 16, y * 8 - 8, face);
             }
         }
     }
@@ -85,6 +71,28 @@ void addBlock(uint8_t x, uint8_t y, uint8_t block) {
     SCREEN[x][y+2].face = 5;
     SCREEN[x][y+1].block = block;
     SCREEN[x][y+1].face = 6;
+}
+
+void initScreen() {
+    for (uint8_t x = 0; x < 9; x++) {
+        for (uint8_t y = 0; y < 14; y++) {
+            addBlock(x*2+1, y*2+1, 0);
+        }
+    }
+}
+
+void drawScreen() {
+    for (uint8_t x = 0; x < SCREEN_WIDTH; x++) {
+        for (uint8_t y = 0; y < SCREEN_HEIGHT; y++) {
+            uint8_t block = SCREEN[x][y].block;
+            uint8_t face = SCREEN[x][y].face;
+            if (block != 0) {
+            	drawSpriteCut(TEXTURES[block - 1], x * 16, y * 8 - 8, face);
+            } else {
+                drawSpriteCut(0, x * 16, y * 8 - 8, face);
+            }
+        }
+    }
 }
 
 void mapToScreen() {
